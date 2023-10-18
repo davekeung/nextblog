16730d22850d9e594bbead118c346da7f5de0cc2
Dave KEUNG <dave.keung@speedyservices.com>
2023-10-18 09:09:47 +0100

    new file:   app/dashboard/user/page.js 	modified:   app/globals.css 	modified:   app/login/page.js 	modified:   components/TopNav.js 	new file:   middleware.js

---
+++ app/dashboard/user/page.js
@@ -0,0 +1,13 @@
+export default function UserDashboard() { 
+    return ( 
+      <div className="container"> 
+        <div className="row"> 
+          <div className="col"> 
+            <p>Dashboard</p> 
+            <hr /> 
+            ... 
+          </div> 
+        </div> 
+      </div> 
+    );
+} 
